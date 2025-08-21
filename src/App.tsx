d5865d67de3c9d70ef1dd15c1a12632df3ee17ee
Paulo Junqueira <48892662+opaulojunqueira@users.noreply.github.com>
2025-08-21 14:58:41 -0300

    mod: routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Tutorial from "./pages/Tutorial";
 import About from "./pages/About";
 import Status from "./pages/Status";
 import Contact from "./pages/Contact";
 import FAQ from "./pages/FAQ";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -26,7 +25,7 @@ const App = () => (
           <Route path="/status" element={<Status />} />
           <Route path="/contato" element={<Contact />} />
           <Route path="/faq" element={<FAQ />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
